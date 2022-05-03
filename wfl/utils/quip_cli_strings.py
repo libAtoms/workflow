eebf7772522f3617b3d7f77331924002f9478696
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2022-05-03 11:24:21 -0400

    Fix behavior of dict_to_gap_fit_string in gap_simple.py so that dicts actually become colon separated lists (functionality is actually in wfl.utils.quip_cli_strings

--- wfl/utils/quip_cli_strings.py
+++ wfl/utils/quip_cli_strings.py
@@ -21,6 +21,17 @@ def dict_to_quip_str(d, list_brackets='{}'):
 
     assert len(list_brackets) % 2 == 0
 
+    def _list_join(sep, v):
+        if isinstance(v, str):
+            # strings are iterable but need to be used as is
+            return v
+
+        try:
+            # try treating as an iterable
+            return sep.join([str(vv) for vv in v])
+        except TypeError as exc:
+            return v
+
     string = ''
     for key, val in d.items():
         if isinstance(val, list):
@@ -29,6 +40,8 @@ def dict_to_quip_str(d, list_brackets='{}'):
             string += f'{key}=' + (list_brackets[0:len(list_brackets) // 2] +
                                    ' '.join([str(v) for v in val]) +
                                    list_brackets[len(list_brackets) // 2:])
+        elif isinstance(val, dict):
+            string += f'{key}=' + ':'.join([k + ':' + _list_join(':', v) for k, v in val.items()])
         else:
             # hope that key_val_dict_to_string encodes value properly
             string += key_val_dict_to_str({key: val})
