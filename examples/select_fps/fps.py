f4b96ef2ab98255f1d9b161c1c535110a835873c
Nikhil Bapat <nikhilabhaybapat@gmail.com>
2022-09-23 15:23:57 +0000

    Including pytest and suggested changes

--- examples/select_fps/fps.py
+++ examples/select_fps/fps.py
@@ -3,26 +3,31 @@ In this example, we show how "greedy farthest point sampling (FPS)" can be used 
 To assign a per-config descriptor we calculate the average SOAP vector for every frame in the MD trajectory. The greedy-FPS algorithm would use them to measure similarities across the datapoints and select 10 unique structures. Overall this requires two input files: the database and the descriptors ("md.xyz" and "gap_params.yaml")
 Tip: gap_params.yaml can either be self-written or automatically generated from a template processed by multi-stage gap fit (wfl.fit.gap.multistage)
 '''
-
+import os
 from wfl.configset import ConfigSet, OutputSpec
 from wfl.descriptors.quippy import calc as calc_descriptors
 from wfl.select.by_descriptor import greedy_fps_conf_global as get_fps
 import yaml
 
-# Step 1: Assign descriptors to the database
-md        = ConfigSet(input_files="md.xyz")
-md_desc   = OutputSpec(output_files="md_desc.xyz")
+def main(nsamples):
+    workdir = os.path.join(os.path.dirname(__file__))
+    # Step 1: Assign descriptors to the database
+    md        = ConfigSet(input_files=os.path.join(workdir, "md.traj"))
+    md_desc   = OutputSpec(output_files=os.path.join(workdir, "md_desc.xyz"))
+    
+    with open(os.path.join(workdir, 'gap_params.yaml'), 'r') as foo:
+        desc_dict = yaml.safe_load(foo)
+    desc_dicts = [d for d in desc_dict if 'soap' in d.keys()] # filtering out only SOAP descriptor
+    for param in desc_dicts:
+        if 'average' not in param.keys():
+            param['average']= True # to create global (per-conf) descriptors instead of local (per-atom)
+    
+    md_desc = calc_descriptors(inputs=md, outputs=md_desc, descs=desc_dicts, key='desc')
+    
+    # Step 2: Sampling
+    fps     = OutputSpec(output_files=os.path.join(workdir, "out_fps.xyz"))
+    get_fps(inputs=md_desc, outputs=fps, num=nsamples, at_descs_info_key='desc', keep_descriptor_info=False)
+    return None
 
-desc_dict = yaml.safe_load(open('gap_params.yaml', 'r'))
-desc_dicts = [d for d in desc_dict if 'soap' in d.keys()] # filtering out only SOAP descriptor
-for param in desc_dicts:
-    if 'average' not in param.keys():
-        param['average']= True # to create global (per-conf) descriptors instead of local (per-atom)
-
-calc_descriptors(inputs=md, outputs=md_desc, descs=desc_dicts, key='desc')
-
-# Step 2: Sampling
-md_desc = ConfigSet(input_files="md_desc.xyz")
-fps     = OutputSpec(output_files="fps.xyz")
-
-get_fps(inputs=md_desc, outputs=fps, num=10, at_descs_info_key='desc', keep_descriptor_info=False)
+if __name__ == '__main__':
+    main(nsamples=8)
---
+++ tests/test_example_fps.py
@@ -0,0 +1,22 @@
+import os, shutil, sys
+from ase.io import read
+
+def test_fps(n=10):
+    example_dir = os.path.join(
+            os.path.dirname(__file__), '../', 'examples', 'select_fps' 
+            )
+    sys.path.append(example_dir)
+    import fps 
+    fps.main(nsamples=n)
+
+    assert os.path.exists(os.path.join(example_dir, 'md_desc.xyz'))
+    assert os.path.exists(os.path.join(example_dir, 'out_fps.xyz'))
+    
+    fps = read(os.path.join(example_dir, "out_fps.xyz"), ":")
+    assert len(fps) == 10
+
+    os.remove(os.path.join(example_dir, 'md_desc.xyz'))
+    os.remove(os.path.join(example_dir, 'out_fps.xyz'))
+       
+if __name__ == '__main__':
+    test_fps()
