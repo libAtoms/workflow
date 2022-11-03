a50b615d49ece421f11e34b701818e59109bd541
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2022-11-03 10:50:08 -0400

    bug in batch_gap_fit fix

--- examples/iterative_gap_fit/batch_gap_fit.py
+++ examples/iterative_gap_fit/batch_gap_fit.py
@@ -70,7 +70,6 @@ def get_ref_error(in_file, out_file, gap_file, **kwargs):
     calc_in_config = generic_calc(in_config, OutputSpec(), calculator, output_prefix='calc_')
     error, _, _ = ref_error_calc(calc_in_config, 'calc_', ref_property_prefix,
                                  category_keys=category_keys)
-    error = error['_ALL_']
     error = {'energy': error['energy/atom']['_ALL_']['RMSE'],
              'forces': error['forces']['_ALL_']['RMSE']}
     return error
