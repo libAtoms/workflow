4481f7a0f9993d24a0dcc4ccd5c63a90624db55b
gelzinyte <elena.gelzinyte@gmail.com>
2023-03-01 16:28:18 +0000

    turn on CI testing

--- tests/test_doc_examples.py
+++ tests/test_doc_examples.py
@@ -22,6 +22,7 @@ def _get_coding_blocks(nb_file):
         pytest.param('examples.select_fps.ipynb', 'all', id='select fps'),
         pytest.param('examples.fhiaims_calculator.ipynb', 'all', id='fhiaims_calculator',
             marks=aims_prerequisites),
+        pytest.param("examples.daisy_chain_mlip_fitting.ipynb", "all", id="daisy_chain_mlip_fitting.ipynb")
     )
 )
 def test_example(tmp_path, nb_file, idx_execute):
@@ -33,3 +34,5 @@ def test_example(tmp_path, nb_file, idx_execute):
     os.chdir(tmp_path)
     exec(code)
     os.chdir('..')
+
+
