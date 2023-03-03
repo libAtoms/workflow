1c1c7dbc1a472e01cc4e9726a94a8c8618eeacbc
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2023-03-03 15:13:26 -0500

    Fix handling of globals in pytest test_example code block exec, and improved error reporting when exec raises an exception

--- tests/test_doc_examples.py
+++ tests/test_doc_examples.py
@@ -25,14 +25,22 @@ def _get_coding_blocks(nb_file):
         pytest.param("examples.daisy_chain_mlip_fitting.ipynb", "all", id="daisy_chain_mlip_fitting")
     )
 )
-def test_example(tmp_path, nb_file, idx_execute):
+def test_example(tmp_path, nb_file, idx_execute, monkeypatch):
+    print("running test_example", nb_file)
     basepath = os.path.join(f'{os.path.dirname(__file__)}/../docs/source')
     coding_blocks = _get_coding_blocks(f'{basepath}/{nb_file}')
     code = '\n'.join([cb_i for idx_i, cb_i in enumerate(coding_blocks) if idx_execute == 'all' or idx_i in idx_execute])
     assert code is not ''
 
-    os.chdir(tmp_path)
-    exec(code)
-    os.chdir('..')
+    monkeypatch.chdir(tmp_path)
+    try:
+        exec(code, globals())
+    except Exception as exc:
+        import traceback, re
+        tb_str = traceback.format_exc()
+        line_nos = list(re.findall("line ([0-9]+),", tb_str))
+        line_no = int(line_nos[-1])
+        lines = list(enumerate(code.splitlines()))[line_no - 5 : line_no + 5]
+        actual_error = "\n".join([f"{li:4d}{'*' if li == line_no else ' '} {l}" for li, l in lines])
 
-
+        raise RuntimeError(f"Exception raised by test_example {nb_file}, traceback:\n{actual_error}\n") from exc
