43e0068f68146385831b660f58217b6b197dfa7d
hjung <hjung@fhi.mpg.de>
2023-10-18 22:31:57 +0200

    modifications in mace pytest

--- tests/test_mace_fitting.py
+++ tests/test_mace_fitting.py
@@ -18,30 +18,32 @@ mace_fit_cmd = f"python {script_dir}/scripts/run_train.py"
 def test_mace_fit_from_list(request, tmp_path, monkeypatch, mace_fit_cmd=mace_fit_cmd):
 
     monkeypatch.chdir(tmp_path)
-    print('getting fitting data from ', request.fspath)
-    print("here :  ", Path(request.fspath))
-
-    params_file_path = Path(Path(request.fspath).parents[0] / 'assets' / 'mace_fit_parameters.yaml')
+    print('getting fitting data from ', request.path)
+    print("here :  ", request.path)
+	
+    parent_path = request.path.parent
+    params_file_path = parent_path / 'assets' / 'mace_fit_parameters.yaml'
     mace_fit_params = yaml.safe_load(params_file_path.read_text())
-    filepath = Path(request.fspath).parents[0] / 'assets' / 'B_DFT_data.xyz'
+    filepath = parent_path / 'assets' / 'B_DFT_data.xyz'
     fitting_configs = ConfigSet(ase.io.read(filepath, ":"))
 
     t0 = time.time()
-    fit(fitting_configs, "test", mace_fit_params, mace_fit_cmd=mace_fit_cmd, run_dir=".", fit_cfgs_fname="B_DFT_data.xyz")
+    fit(fitting_configs, "test", mace_fit_params, mace_fit_cmd=mace_fit_cmd, run_dir=".")
     t_run = time.time() - t0
 
-    assert Path.exists(Path(tmp_path / f"test.model"))
+    assert Path.exists(tmp_path / f"test.model")
     assert Path(tmp_path / f"test.model").stat().st_size > 0
 
 
 def test_mace_fit(request, tmp_path, monkeypatch, mace_fit_cmd=mace_fit_cmd):
 
     monkeypatch.chdir(tmp_path)
-    print('getting fitting data from ', request.fspath)
-    print("here :  ", Path(request.fspath))
+    print('getting fitting data from ', request.path)
+    print("here :  ", request.path)
 
-    fit_config_file = Path(Path(request.fspath).parents[0] / 'assets' / 'B_DFT_data.xyz')
-    params_file_path = Path(Path(request.fspath).parents[0] / 'assets' / 'mace_fit_parameters.yaml')
+    parent_path = request.path.parent
+    fit_config_file = parent_path / 'assets' / 'B_DFT_data.xyz'
+    params_file_path = parent_path / 'assets' / 'mace_fit_parameters.yaml'
     mace_fit_params = yaml.safe_load(params_file_path.read_text())
     mace_fit_params["train_file"] = fit_config_file
     fitting_configs = ConfigSet(fit_config_file) 
@@ -50,7 +52,7 @@ def test_mace_fit(request, tmp_path, monkeypatch, mace_fit_cmd=mace_fit_cmd):
     fit(fitting_configs, "test", mace_fit_params, mace_fit_cmd=mace_fit_cmd, run_dir=".")
     t_run = time.time() - t0
 
-    assert Path.exists(Path(tmp_path / f"test.model"))
+    assert Path.exists(tmp_path / f"test.model")
     assert Path(tmp_path / f"test.model").stat().st_size > 0
 
 
