2490cf81f4ed8085efcb4b736f7b23dc20521f7c
jungsdao <hjung@fhi.mpg.de>
2024-03-02 15:28:07 +0000

    some polishing

--- wfl/generate/neb.py
+++ wfl/generate/neb.py
@@ -1,53 +1,31 @@
 import sys
 
-import ase.units
 import numpy as np
-import spglib
-from ase.constraints import ExpCellFilter
-#from ase.optimize.precon import PreconFIRE
 from ase.optimize import FIRE
-from ase.neb import NEB
-from ase.dyneb import DyNEB
+from ase.mep.dyneb import DyNEB
 
 from wfl.autoparallelize import autoparallelize, autoparallelize_docstring
 from wfl.utils.at_copy_save_results import at_copy_save_results
 from wfl.utils.misc import atoms_to_list
 from wfl.utils.parallel import construct_calculator_picklesafe
-from wfl.utils.pressure import sample_pressure
 from .utils import config_type_append
 
-orig_log = FIRE.log
 
-
-def _new_log(self, forces=None):
-    if 'buildcell_config_i' in self.atoms.info:
-        if self.logfile is not None:
-            self.logfile.write(str(self.atoms.info['buildcell_config_i']) + ' ')
-    orig_log(self, forces)
-
-    try:
-        self.logfile.flush()
-    except:
-        pass
-
-
-#PreconFIRE.log = _new_log
-
-
-def _run_autopara_wrappable(list_of_images, calculator, fmax=1.0e-3, smax=None, steps=1000, pressure=None,
-           keep_symmetry=True, traj_step_interval=1, traj_subselect=None, skip_failures=True,
-           results_prefix='optimize_', verbose=False, update_config_type=True,
+def _run_autopara_wrappable(list_of_images, calculator, fmax=5e-2, steps=1000,
+           traj_step_interval=1, traj_subselect=None, skip_failures=True,
+           results_prefix='neb_', verbose=False, logfile=None, update_config_type=True,
            autopara_rng_seed=None, autopara_per_item_info=None,
-           **opt_kwargs):
+           **neb_kwargs):
     """runs a structure optimization
 
     Parameters
     ----------
-    list_of_images: list(Atoms)
-        input configs
+    list_of_images: list(images)
+        input configs. images are list(Atoms)
+        (i.e. [images1, images2, images3])
     calculator: Calculator / (initializer, args, kwargs)
         ASE calculator or routine to call to create calculator
-    fmax: float, default 1e-3
+    fmax: float, default 5e-2
         force convergence tolerance
     steps: int, default 1000
         max number of steps
@@ -63,8 +41,8 @@ def _run_autopara_wrappable(list_of_images, calculator, fmax=1.0e-3, smax=None, 
         optimisation logs are not printed unless this is True
     update_config_type: bool, default True
         append at.info['optimize_config_type'] at.info['config_type']
-    opt_kwargs
-        keyword arguments for PreconFIRE
+    neb_kwargs
+        keyword arguments for DyNEB and FIRE 
     autopara_rng_seed: int, default None
         global seed used to initialize rng so that each operation uses a different but
         deterministic local seed, use a random value if None
@@ -73,11 +51,10 @@ def _run_autopara_wrappable(list_of_images, calculator, fmax=1.0e-3, smax=None, 
     -------
         list(Atoms) trajectories
     """
-    opt_kwargs_to_use = dict(logfile=None, master=True)
-    opt_kwargs_to_use.update(opt_kwargs)
-
-    if opt_kwargs_to_use.get('logfile') is None and verbose:
-        opt_kwargs_to_use['logfile'] = '-'
+    logfile = neb_kwargs.get("logfile", None)
+   
+    if logfile is None and verbose:
+        logfile = "-"
 
     calculator = construct_calculator_picklesafe(calculator)
 
@@ -93,20 +70,12 @@ def _run_autopara_wrappable(list_of_images, calculator, fmax=1.0e-3, smax=None, 
     
             at.calc = calculator
 
-    neb = DyNEB(images, k=0.2, climb=True, allow_shared_calculator=True, scale_fmax = 1)
-    opt = FIRE(neb, **opt_kwargs_to_use)
-
-        # default status, will be overwritten for first and last configs in traj
-#        at.info['optimize_config_type'] = 'optimize_mid'
+    neb = DyNEB(images, **neb_kwargs)
+    opt = FIRE(neb, logfile=logfile)
 
     traj = []
     def process_step():
 
-#            if len(traj) > 0 and traj[-1] == at:
-#                # Some optimization algorithms sometimes seem to repeat, perhaps
-#                # only in weird circumstances, e.g. bad gradients near breakdown.
-#                # Do not store those duplicate configs.
-#                return
         subtraj = []
         for at in images:
             new_config = at_copy_save_results(at, results_prefix=results_prefix)
@@ -129,18 +98,12 @@ def _run_autopara_wrappable(list_of_images, calculator, fmax=1.0e-3, smax=None, 
         # that should probably never happen
         final_status = 'exception'
         if skip_failures:
-            sys.stderr.write(f'Structure optimization failed with exception \'{exc}\'\n')
+            sys.stderr.write(f'Nudged elastic band calculation failed with exception \'{exc}\'\n')
             sys.stderr.flush()
         else:
             raise
 
-#    if len(traj) == 0 or traj[-1] != at:
-#        new_config = at_copy_save_results(at, results_prefix=results_prefix)
-#        new_config.set_constraint(org_constraints)
-#        traj.append(new_config)
-
     # set for first config, to be overwritten if it's also last config
-#    print("traj : ", traj)
 
     for at in traj[0]:
         at.info['neb_config_type'] = 'neb_initial'
@@ -178,14 +141,9 @@ def NEB(*args, **kwargs):
 
     return autoparallelize(_run_autopara_wrappable, *args,
                            default_autopara_info=default_autopara_info, **kwargs)
-#autoparallelize_docstring(NEB, _run_autopara_wrappable, "Atoms")
+autoparallelize_docstring(NEB, _run_autopara_wrappable, "Atoms")
 
 
-# Just a placeholder for now. Could perhaps include:
-#    equispaced in energy
-#    equispaced in Cartesian path length
-#    equispaced in some other kind of distance (e.g. SOAP)
-# also, should it also have max distance instead of number of samples?
 def subselect_from_traj(traj, subselect=None):
     """Sub-selects configurations from trajectory.
 
