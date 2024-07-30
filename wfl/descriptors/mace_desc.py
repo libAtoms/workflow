a2a158f730cbfe3e6de8e7c1e1f31f7cd9e8e22c
hjung <hjung@fhi.mpg.de>
2024-07-30 18:59:26 +0000

    small change

--- wfl/descriptors/mace_desc.py
+++ wfl/descriptors/mace_desc.py
@@ -19,7 +19,7 @@ def _calc_autopara_wrappable(atoms, calculator, key, invariants_only=True, per_a
         key in Atoms.info (``not per_atom``) or Atoms.arrays (``per_atom``) to store information
     invariants_only : bool, default True
         get only invariant part of the descriptor
-    per_atom: bool
+    per_atom: bool, default False
         calculate a local (per-atom) descriptor, as opposed to global (per-config)
     normalize: bool, default True
         normalize final vector (e.g. if contributions from multiple descriptors were concatenated)
