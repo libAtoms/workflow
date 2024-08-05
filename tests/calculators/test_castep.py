98decdbb5f88138e5763fbc250d26c44a8af6849
Elena <elena.gelzinyte@gmail.com>
2024-08-05 10:41:34 +0200

    loosen castep test tolerances when running a different castep version on a different machine

--- tests/calculators/test_castep.py
+++ tests/calculators/test_castep.py
@@ -30,9 +30,9 @@ def test_castep_calculation(tmp_path):
     )
 
     atoms.calc = calc
-    assert atoms.get_potential_energy() == approx(-217.2263559019)
-    assert atoms.get_forces() == approx(np.array([[-0., -0.,  0.], [ 0.,  0., -0.]]))
-    assert atoms.get_stress() ==  approx(np.array([ 0.06361731, 0.06361731, 0.06361731,-0., 0., 0.]))
+    assert atoms.get_potential_energy() == approx(-217.2263559019, 2e-3)
+    assert atoms.get_forces() == approx(np.array([[-0., -0.,  0.], [ 0.,  0., -0.]]), abs=1e-4)
+    assert atoms.get_stress() ==  approx(np.array([ 0.06361731, 0.06361731, 0.06361731,-0., 0., 0.]), abs=1e-5)
 
 
 def test_castep_calc_via_generic(tmp_path):
